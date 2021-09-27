9f81fcc5a2ef6071a7cd8b5f47bf03058914ac93
suwa <baru.web.production@gmail.com>
2021-09-28 08:30:01 +0900

    userの一覧を返すgetアクションを作成する

--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,15 @@ require 'grape'
 
 class User < Grape::API
   format :json
+  @@users = []
+
+  get '/users' do
+    if @@users.empty?
+      status 404
+      { message: 'Not Found User' }
+    else
+      status 200
+      @@users
+    end
+  end
 end
