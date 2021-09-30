5a0eb9be3d833cfaeedf0ae64dbd7d761e0b50d6
suwa <baru.web.production@gmail.com>
2021-09-30 20:43:09 +0900

    GET /usersのユーザーが存在しない場合のテストを作成する

--- spec/lib/users_spec.rb
+++ spec/lib/users_spec.rb
@@ -10,9 +10,21 @@ RSpec.describe Users do
 
   describe 'GET /' do
     context 'レスポンスが成功した場合' do
-      it 'ステータスコードが302を返すこと' do
+      it '302ステータスコードを返すこと' do
         get '/'
         expect(last_response.status).to eq 302
+      end
+    end
+  end
+
+  describe 'GET /users' do
+    before do
+      get '/users'
+    end
+
+    context 'ユーザーが存在しない場合' do
+      it '404ステータスコードを返すこと' do
+        expect(last_response.status).to eq 404
       end
     end
   end
