79bb8ead9e9dd18b13cda0ba90dcf67fa314eda5
suwa <baru.web.production@gmail.com>
2021-09-30 20:36:12 +0900

    GET /のテストを作成する

--- spec/lib/users_spec.rb
+++ spec/lib/users_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
+require 'rack/test'
 
 RSpec.describe Users do
-  
+  include Rack::Test::Methods
+
+  def app
+    Users.new
+  end
+
+  describe 'GET /' do
+    context 'レスポンスが成功した場合' do
+      it 'ステータスコードが302を返すこと' do
+        get '/'
+        expect(last_response.status).to eq 302
+      end
+    end
+  end
 end
